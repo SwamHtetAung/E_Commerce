a480918aa2d16d63df9907fcfeea636535737c3b
SwamHtetAung <65816187+SwamHtetAung@users.noreply.github.com>
2020-06-18 09:01:31 +0630

    E_commerce Project

--- app/src/main/java/com/shaung/txt/e_commerce/CategoryActivity.kt
+++ app/src/main/java/com/shaung/txt/e_commerce/CategoryActivity.kt
@@ -3,7 +3,9 @@ package com.shaung.txt.e_commerce
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.shaung.txt.e_commerce.libby.H.Companion.userAuth
+import kotlinx.android.synthetic.main.activity_category.*
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,7 @@ class CategoryActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+
+        categoryRecycler.layoutManager = GridLayoutManager(this, 2)
     }
 }
